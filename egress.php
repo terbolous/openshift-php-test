eddfaf84e05df7fbad2ae19790161d84e968742f
Erik Weber <erik@vangenplotz.no>
2018-10-11 10:30:05 +0200

    Update egress test method

--- egress.php
+++ egress.php
@@ -1,7 +1,22 @@
 <?php
 
-$example= @file_get_contents('http://www.example.com/');
+function do_check($url) {
+    $ch=curl_init();
+    $timeout=5;
+    
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_TIMEOUT, $timeout);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+    
+    $result=curl_exec($ch);
+    curl_close($ch);
+    return $result;
+}
+
+
+$example= do_check('http://www.example.com/');
 echo 'example: '. gettype($example) .'<br />';
 
-$vg = @file_get_contents('https://www.vg.no');
+$vg = do_check('https://www.vg.nosdf');
 echo 'vg: ' . gettype($vg) .'<br />'; 
